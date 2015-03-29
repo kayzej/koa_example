99611cb7c1cf09af51a39918ba212dbb04613c73
Jeffrey Kayzerman <kayzej@gmail.com>
2015-03-29 18:52:26 -0400

    third commit

--- koa_test/src/js/app.js
+++ koa_test/src/js/app.js
@@ -7,17 +7,34 @@ var App = {
 	bindUIActions: function(){ 
 		console.log('Binding UI Actions');
 
-		$('#myButton').click(function(){ 
+		$('#listbtn').click(function(){ 
 			console.log('You hit the button');
+			
+			$.get('/list', function(data){
+				console.log(data);
+			});
 		});
-	},
 
-	get: function(){ 
-		console.log('Get Function')
+		// $('#form-submit').click(function(){
+		// 	console.log('submitting form');
+
+		// 	var data = $( "#testform" ).serialize(); 
+		// 	console.log('posting data: ' + data );
+
+		// 	// $.post('/insert', data, function(data){
+		// 	// 	console.log('succesfully posted to server');
+		// 	// }, "json");
+		// });
+
+		$('#test-form').submit(function(event){
+			event.preventDefault();
+			console.log('form submitted');
+		});
 	}
 }
 
 $(document).ready(function(){ 
 	App.init();
+	App.bindUIActions();
 });
 }());
